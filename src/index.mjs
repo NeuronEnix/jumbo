f6fce8fb43d869b9a0511bd82df13f310b3f386c
Kaushik R Bangera <kaushikrb909@gmail.com>
2024-08-03 19:13:11 +0530

    feat(index): added index.js with cors, helmet, logger and route

---
+++ src/index.mjs
@@ -0,0 +1,41 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
+import { resOk, resErr } from "./common/respond.mjs"
+import CONFIG from "./common/config.mjs"
+import appRouter from "./api/index.mjs"
+import { connectToDatabase} from "./dbConnection.mjs"
+import { inbound } from './common/logger.mjs';
+
+const app = express();
+connectToDatabase()
+
+app.use(helmet())
+app.use(cors({
+  origin: '*', // ["https://jumbo.kaushikrb.com"]
+  methods: '*', // ["GET", "POST", "OPTIONS"]
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: '*', // ["Content-Type", "Authorization"]
+}));
+
+app.use(bodyParser.json({limit: '1mb'}));
+app.use(inbound) // To log request
+
+app.use(appRouter) // To handle request
+
+app.get("/ping", (req, res) => {
+  resOk(res, { ping: "pong", date: new Date() })
+});
+
+app.use((err, req, res, next) => {
+  // handle invalid json
+  if (err instanceof SyntaxError) {
+    return resErr.gen.invalidRequestData().send(res);
+  }
+});
+
+app.listen(CONFIG.SERVER.PORT, () => {
+  console.log(`Server at: http://localhost:${CONFIG.SERVER.PORT}`);
+});
