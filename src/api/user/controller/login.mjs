eb2f471169252c4bc68a79cbd90cedfa27886e9c
Kaushik R Bangera <kaushikrb909@gmail.com>
2024-08-03 19:45:20 +0530

    feat(api): user > login > added login route with refreshToken

---
+++ src/api/user/controller/login.mjs
@@ -0,0 +1,72 @@
+import Ajv from 'ajv';
+import addFormats from "ajv-formats"
+
+import { compare as comparePass } from 'bcrypt';
+
+import { UserSchema, UserDao } from '../../../model/user.mjs';
+import { resErr, resOk, ResponseError } from '../../../common/respond.mjs';
+import { STATUS, AUTH } from '../../../common/const.mjs';
+import { getNewRefreshToken } from '../../../lib/auth.mjs';
+
+const ajv = new Ajv({ allErrors: true });
+addFormats(ajv)
+
+const schema = {
+  type: 'object',
+  properties: {
+    email: UserSchema.email,
+    pass: UserSchema.pass,
+  },
+  required: ['email', 'pass'],
+  additionalProperties: false,
+};
+
+const ajvValidator = ajv.compile(schema);
+
+async function validate(req, res, next) {
+  const dataValidated = ajvValidator(req.body);
+  if (!dataValidated) {
+    return resErr.gen.invalidParam('', ajvValidator.errors).send(res);
+  }
+  next()
+}
+
+
+export async function execute(req, res) {
+  const data = req.body;
+  try {
+
+    const user = await UserDao.findOne(
+      { email: data.email },
+      { email: 1, pass: 1, status: 1 }
+    )
+
+    if (!user) throw resErr.auth.invalidCredential();
+    if (user.status !== STATUS.ACTIVE) throw resErr.auth.userInactive();
+
+    const isPassOk = await comparePass(data.pass, user.pass);
+    if (!isPassOk) throw resErr.auth.invalidCredential();
+
+    // Create new refresh token
+    const tokenObj = await getNewRefreshToken(user.id);
+    user.refTokJti = tokenObj.refreshTokenPayload.jti;
+    await user.save({ fields: ['refTokJti'] });
+
+    // Attach refresh token to cookie marked http only
+    const refreshTokenCookieProperty = { maxAge: AUTH.REFRESH_TOKEN_EXPIRE_COOKIE, httpOnly: true };
+    res.cookie('refreshToken', tokenObj.refreshToken, refreshTokenCookieProperty);
+
+    return resOk(res, { userId: tokenObj.userId });
+
+  } catch (e) {
+    if (e instanceof ResponseError) {
+      return e.send(res);
+    }
+    return resErr.gen.unknown(e).send(res);
+  }
+}
+
+export default {
+  validate,
+  execute,
+}
--- src/api/user/index.mjs
+++ src/api/user/index.mjs
@@ -2,9 +2,12 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 
 import register from './controller/register.mjs';
+import login from './controller/login.mjs';
 
 const router = express.Router();
 
 router.post('/register', register.validate, register.execute);
+router.post('/login', login.validate, login.execute);
+
 
 export default router
