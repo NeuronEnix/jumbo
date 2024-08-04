f188cae92667042af978df3a537c9c2d46fe0558
Kaushik R Bangera <kaushikrb909@gmail.com>
2024-08-04 09:22:19 +0530

    refactor(websocket): gracefully disconnect on token error

--- src/webSocket.mjs
+++ src/webSocket.mjs
@@ -2,6 +2,8 @@ import WebSocket, { WebSocketServer } from 'ws';
 import CONFIG from './common/config.mjs';
 import { verifyAccessToken } from './lib/auth.mjs';
 import { ResponseError } from './common/respond.mjs';
+import { GAME_EVENT } from './common/const.mjs';
+import { handleAnswerSubmitEvent } from './lib/wsEvent.mjs';
 
 export const userWs = {}
 
@@ -15,15 +17,44 @@ wss.on('connection', (ws, req) => {
   try {
     const tokenPayload = verifyAccessToken(req.headers.authorization)
     userWs[tokenPayload.id] = ws
-    ws.on('message', (data) => {
-      console.log(data)
+    ws.userId = tokenPayload.id
+    ws.on('message', async (data) => {
+      try {
+
+        data = JSON.parse(data.toString())
+        if (!data?.event) return sendEvent(ws, { event: 'ERROR', data: { msg: 'Accepts only JSON' } })
+        console.log(`WS: ${data.event}`)
+
+        switch (data?.event) {
+          case GAME_EVENT.ANSWER_SUBMIT.name: await handleAnswerSubmitEvent(data); break;
+        }
+
+      } catch (e) {
+        if (e instanceof ResponseError)
+          return sendEvent(ws, { event: 'ERROR', code: e.code, msg: e.msg, data: e.data })
+        console.log(e)
+        if (!data?.event) ws.send(JSON.stringify({ event: 'ERROR', data: { msg: 'Accepts only JSON' } }))
+      }
     })
   } catch (e) {
-    if ( e instanceof ResponseError ) {
-      console.log(e.msg)
+    if (e instanceof ResponseError) {
+      sendEvent(ws, { event: 'ERROR', code: e.code, msg: e.msg, data: e.data })
+      if (['INVALID_TOKEN', 'EXPIRED_TOKEN'].includes(e.code)) {
+        disconnect(ws)
+      }
+    } else {
+      disconnect(ws)
     }
-    ws.close()
   }
 });
 
 export const isUserConnected = (userId) => userWs[userId]
+const sendEvent = (ws, data) => ws.send(JSON.stringify(data))
+const disconnect = (ws) => {
+  const userId = ws.userId
+  if (userId) {
+    delete userWs[userId]
+  }
+  ws.close()
+}
+export const sendEventToUser = (userId, data) => userWs[userId] && userWs[userId].send(JSON.stringify(data))
