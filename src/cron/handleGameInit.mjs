56ee07d79d4168a79b315382f0d149f4798b1b0d
Kaushik R Bangera <kaushikrb909@gmail.com>
2024-08-04 15:36:18 +0530

    feat(cron): gameTimeout handler added to end timed out sessions

--- src/cron/handleGameInit.mjs
+++ src/cron/handleGameInit.mjs
@@ -1,12 +1,14 @@
-import { GAME_CONFIG } from "../common/const.mjs";
+import { GAME_CONFIG, STATUS } from "../common/const.mjs";
 import { resErr } from "../common/respond.mjs";
-import { sendGameInit, sendQuestionSendEvent } from "../lib/wsEvent.mjs";
+import { sendGameEndEvent, sendGameInit, sendQuestionSendEvent } from "../lib/wsEvent.mjs";
 import { GameSessionDao } from "../model/gameSession.mjs";
 import { QuestionDao } from "../model/question.mjs";
+import { UserDao } from "../model/user.mjs";
 
 let counter = 0;
 const prevVal = {
-  userListLen: -1
+  userListLen: -1,
+  gameSessionCount: -1
 }
 
 /**
@@ -26,7 +28,7 @@ const userIdSet = new Set();
  * @param userObj {T_UserObj}
  */
 export function addToQueue(userObj) {
-  if (userIdSet.has(userObj)) {
+  if (userIdSet.has(userObj.userId)) {
     throw resErr.game.alreadyInQueue();
   }
   userList.push(userObj);
@@ -47,11 +49,11 @@ export async function handleGameInit() {
       users: gameUsers
     })
     gameUsers.forEach((u) => {
-      sendQuestionSendEvent( u.userId, gameSession.gameSessionId, {
+      sendQuestionSendEvent(u.userId, gameSession.gameSessionId, {
         _id: gameSession.questionList[0]._id,
         text: gameSession.questionList[0].questionText,
         options: gameSession.questionList[0].options
-      } )
+      })
     });
   }
   setTimeout(handleGameInit, 1000);
@@ -66,10 +68,45 @@ async function createGameSession(userList) {
   const questionList = await getRandomQuestion(GAME_CONFIG.MAX_QUESTION);
   const gameSession = await GameSessionDao.create({
     questionIds: questionList,
+    timeoutAt: Date.now() + GAME_CONFIG.MAX_GAME_TIME_MS,
     users: userList.map((u) => ({ _id: u.userId })),
   })
+  await UserDao.updateMany({ _id: { $in: userList.map((u) => u.userId) } }, { $set: { gameSessionId: gameSession._id } })
   return {
     gameSessionId: gameSession._id,
     questionList
   }
+}
+
+let timeoutCounter = 0
+export async function handleGameTimeout() {
+  const gameSessions = await GameSessionDao.find({ status: STATUS.ACTIVE });
+  ++timeoutCounter
+  if (gameSessions.length !== prevVal.gameSessionCount) {
+    console.log(`handleGameTimeout(${timeoutCounter}): ${gameSessions.length} gameSessions`)
+    prevVal.gameSessionCount = gameSessions.length
+  }
+  gameSessions.forEach(async gs => {
+    if (gs.timeoutAt > Date.now()) {
+      const questionList = await QuestionDao.find({ _id: { $in: gs.questionIds } });
+      gs.users.forEach(gameUser => {
+        gameUser.gameSubmitted = true;
+        gameUser.score = gameUser.answers.reduce((prev, cur) => {
+          const question = questionList.find((q) => q._id.equals(cur.questionId));
+          if (question.correctOptionId == cur.chosenOptionId) prev += 1
+          return prev
+        }, 0)
+      })
+      gs.status = STATUS.COMPLETED;
+      gs.save()
+        .catch((err) => { console.log(err) })
+
+      UserDao.updateMany(
+        { _id: { $in: gs.users.map((u) => u._id) } },
+        { $set: { gameSessionId: null } }
+      ).catch((err) => { console.log(err) })
+      sendGameEndEvent(gs)
+    }
+  })
+  setTimeout(handleGameTimeout, 5000);
 }
